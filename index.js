c5c665014df1baf4131bca93e6f7fdd5b21e4f7b
Andrew Napoleone <Naps277@gmail.com>
2023-12-09 14:58:04 -0500

    added echo and echo error message

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
